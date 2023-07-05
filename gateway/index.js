719d146e956c1aeae5dd56726d82e998581a672a
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2023-07-04 21:47:33 -0300

    modulo 1

--- gateway/index.js
+++ gateway/index.js
@@ -1,26 +1,35 @@
 const express = require("express");
 const morgan = require("morgan");
-const {createProxyMiddleware} = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
 app.use(morgan("dev"));
 
-app.use( "/characters", createProxyMiddleware({
-    target: "http://localhost:8001",
-    changeOrigin:true,
-}));
+app.use(
+  "/characters",
+  createProxyMiddleware({
+    target: "http://characters:8001",
+    changeOrigin: true,
+  })
+);
 
-app.use( "/films", createProxyMiddleware({
-    target: "http://localhost:8002",
-    changeOrigin:true,
-}));
+app.use(
+  "/films",
+  createProxyMiddleware({
+    target: "http://films:8002",
+    changeOrigin: true,
+  })
+);
 
-app.use( "/planets", createProxyMiddleware({
-    target: "http://localhost:8003",
-    changeOrigin:true,
-}));
+app.use(
+  "/planets",
+  createProxyMiddleware({
+    target: "http://planets:8003",
+    changeOrigin: true,
+  })
+);
 
 app.listen(8000, () => {
-    console.log("gateway on port 8000");
+  console.log("gateway on port 8000");
 });
