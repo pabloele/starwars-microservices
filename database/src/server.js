be16a9c8e64b283c2b9c62a8a7f6850316804e1c
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2023-07-10 16:54:52 -0300

    database routes

--- database/src/server.js
+++ database/src/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const server = express();
 const morgan = require("morgan");
 
-server.use(morgan("dev"));
 server.use(express.json());
+server.use(morgan("dev"));
 
+server.use(require("./routes"));
 module.exports = server;
